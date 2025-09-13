3f4142e1cff56891f58c52f1b84e7067ed13d2df
Aklesiya3 <ekklesiaendalkachew@gmail.com>
2025-09-13 17:44:36 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # ✅ Make sure Library is imported
 
 # Function-based view to list all books
 def list_books(request):
@@ -15,5 +15,6 @@ class LibraryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()  # pass books in the library
+        context['books'] = self.object.books.all()  # include all books in this library
         return context
+
