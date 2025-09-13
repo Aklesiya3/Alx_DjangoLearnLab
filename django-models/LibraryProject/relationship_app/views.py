9e050c707ce67d8a76df9dcd799db194b95cfc68
ekklesia <ekklesiaendalkachew@gmail.com>
2025-09-13 17:29:24 +0300

    "views"

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+# Class-based view using DetailView
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.list_books, name="list_books"),  # function-based
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based
+]
