297521f35a3cceae1b06fea9a578ca72520ce2de
ekklesia <ekklesiaendalkachew@gmail.com>
2025-09-20 00:35:54 +0300

    "api project"

---
+++ api_project/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookViewSet, BookList  
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    # still keep old API for list only
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+]
