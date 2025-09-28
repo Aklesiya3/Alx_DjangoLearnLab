7ac822f1e4c609956f9c4195b6ed23c812af674f
Aklesiya3 <ekklesiaendalkachew@gmail.com>
2025-09-28 21:07:28 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,19 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
-
-class Author (models.Model):
-    # This model stores information about an Author
+# Author model
+class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
-class Book (models.Model):
-     # This model stores information about a Book
-    title=models.CharField(max_length=200)
-    publication_year=models.ImtegerField()
-    # ForeignKey creates the link between Book and Author
-    author= models.Foreignkey(Author, related_name='books', on_delete=models.CASCADE)
 
-    def _str_(self):
+
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.title
+
+    # Optional: Custom method to validate publication_year
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        current_year = timezone.now().year
+        if self.publication_year > current_year:
+            raise ValidationError('Publication year cannot be in the future.')
